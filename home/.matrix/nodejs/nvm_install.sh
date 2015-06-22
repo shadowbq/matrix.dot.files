a0bab1c468c2e86d1cde8786fdae6c9488fbd84f
shadowbq <shadowbq@gmail.com>
2015-06-22 16:01:18 -0400

    setting up io.js as default node

--- home/.matrix/nodejs/nvm_install.sh
+++ home/.matrix/nodejs/nvm_install.sh
@@ -8,11 +8,23 @@ git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git check
 # https://github.com/creationix/nvm
 . ~/.nvm/nvm.sh
 
+# Node.js 0.10.x is stable (ECMA 5.x)
+# Node.js 0.12.x is current but dated ECMA 5.x (3.28.73 that ship with Node.js™ 0.12.x)
+# IO.js is ECMA ES 6.x (V8 4.2.77.20, which includes ES6)
+# Node.js will merge IO.js and IO.js will take over
+# https://github.com/nodejs/dev-policy/blob/master/convergence.md
+
 # Install a 0.10.x Version of NodeJS
-nvm install 0.10
+# nvm install 0.10
+
+# Install io.js Version of NodeJS
+# https://github.com/nodejs/io.js
+nvm install iojs
 
 # Use a Node version
-nvm use 0.10
+# nvm use 0.10
+nvm use iojs
 
 # NVM Defaults (this is also sources on every shell launch)
-nvm alias default 0.10
+# nvm alias default 0.10
+nvm alias default iojs
