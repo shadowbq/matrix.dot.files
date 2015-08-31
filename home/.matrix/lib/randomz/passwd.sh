8fba8f455bc57268170538f4dcc8f04e0c71b885
shadowbq <shadowbq@gmail.com>
2015-08-30 23:46:41 -0400

    Update passwd.sh

--- home/.matrix/lib/randomz/passwd.sh
+++ home/.matrix/lib/randomz/passwd.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+## Should use apg if available
+## http://linux.die.net/man/1/apg
+
 randpw(){ < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16};echo;}
 
 randpw_special(){ < /dev/urandom tr -dc _A-Z-a-z-0-9\\{}\;:_\- | head -c${1:-16};echo;}
